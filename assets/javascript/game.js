fe46ed1ab83f80c74bbb04ab835e999f936c7522
dtroxler21 <dtroxler21@gmail.com>
2018-02-01 14:12:59 -0500

    fixing some bugs...just a few more to go (hopefully)!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,19 +2,21 @@
 var words = ["HAZARD", "ROONEY", "KANE", "POGBA", "BECKHAM", "LAMPARD", "GERRARD", "DROGBA", "HENRY", "AGUERO", "SHEARER", "RONALDO", "SUAREZ", "SILVA", "BERGKAMP", "TERRY", "CECH", "FERDINAND", "VIEIRA", "GIGGS"];
 
 //Creating an array of letters for guesses
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 
 //Creating more variables to be used later (and an object for all html entered via js)
 var randomWord;
-var wins = document.getElementById("win-count");
-var guessesLeft = document.getElementById("guesses-remaining");
+var wins = 0;
+var guessesLeft = 12;
 var underscoreWord = [];
 var guessedLetters = [];
 var guessedAgain = [];
 
 var html = {
+	winCount: document.getElementById("win-count"),
 	currentWord: document.getElementById("word"),
+	guessesRemaining: document.getElementById("guesses-remaining"),
 	lettersGuessed: document.getElementById("letters-guessed"),
 }
 
@@ -32,16 +34,18 @@ function begin() {
 		underscoreWord[i] = "_";
 	}
 
-	//html change at beginning of the game
+	//html changes at beginning of the game
+	html.winCount.innerHTML = wins;
 	html.currentWord.innerHTML = underscoreWord.join(" ");
+	html.guessesRemaining.innerHTML = 12;
+	html.lettersGuessed.innerHTML = " ";
 };
 
 //Winning the game and resetting values to pre-game values
-function winner() {
+function winner(){
 	if (underscoreWord.indexOf("_") === -1) {
 		wins++;
 		underscoreWord = [];
-		guessesLeft = 12;
 		guessedLetters = [];
 		guessedAgain = [];
 		begin();
@@ -49,10 +53,9 @@ function winner() {
 };
 
 //Losing the game and resetting values to pre-game values
-function loser() {
+function loser(){
 	if (guessesLeft === 0) {
 		underscoreWord = [];
-		guessesLeft = 12;
 		guessedLetters = [];
 		guessedAgain = [];
 		begin();
@@ -60,25 +63,40 @@ function loser() {
 };
 
 //User events such as beginning the game and guessing letters
-document.addEventListener('keypress', (event) => {
-	begin();
-	var userGuess = event.key.toUpperCase();
-	if (randomWord.indexOf(userGuess) === -1 && (guessedAgain.indexOf(userGuess) === -1)) {
-		guessedLetters.push(userGuess);
-		guessedAgain.push(userGuess);
-		html.lettersGuessed.innerHTML = guessedLetters.join(" ");
-		guessesLeft--;
-	};
+document.onkeyup = function(event) {
+	if (event.keyCode === 13) {
+		begin();
 
-	if ((randomWord.indexOf(userGuess) > -1) && (guessedAgain.indexOf(userGuess) === -1)) {
-		guessedAgain.push(userGuess);
-		for (var j = 0; j < randomWord.length; j++) {
-			if (randomWord[j] === userGuess) {
-				underscoreWord[j] = userGuess;
+		document.onkeyup = function(event) {
+			var userGuess = event.key.toUpperCase();
+			if (letters.indexOf(userGuess) === -1) {
+				alert ("Choose a letter.");
+			}
+
+			else if (randomWord.indexOf(userGuess) === -1 && (guessedAgain.indexOf(userGuess) === -1)) {
+				guessesLeft--;
+				guessedLetters.push(userGuess);
+				guessedAgain.push(userGuess);
+				html.lettersGuessed.innerHTML = guessedLetters.join(" ");
+				html.guessesRemaining.innerHTML = guessesLeft;
 			};
-		}
 
-		html.currentWord.innerHTML = underscoreWord.join("");
+			if ((randomWord.indexOf(userGuess) > -1) && (guessedAgain.indexOf(userGuess) === -1)) {
+				guessedAgain.push(userGuess);
+				for (var j = 0; j < randomWord.length; j++) {
+					if (randomWord[j] === userGuess) {
+						underscoreWord[j] = userGuess;
+					};
+				}
+
+				html.currentWord.innerHTML = underscoreWord.join("");
+
+				winner();
+				loser();
+			};
+		};
 	};
-});
+};
+
+
 
